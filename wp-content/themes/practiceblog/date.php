eb9de194307db70fdc4d9b3741d6352f2452fb55
Nora <nsorym04@gmail.com>
2023-02-07 18:51:30 +0900

    パーツの共通化

--- wp-content/themes/practiceblog/date.php
+++ wp-content/themes/practiceblog/date.php
@@ -5,33 +5,15 @@
         <h1 class="page-title">
             <?php echo get_query_var('year') . '年' . get_query_var('monthnum') . '月'; ?>
         </h1>
-        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-                <?php $cat = get_the_category();
-                $catname = $cat[0]->cat_name;
-                ?>
-                <article>
-                    <h2 class="article-title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-                    <ul class="meta">
-                        <li><?php the_time('Y/m/d'); ?></li>
-                        <li><?php echo $catname; ?></li>
-                    </ul>
-                    <a href="<?php the_permalink() ?>"><?php the_post_thumbnail(); ?></a>
-                    <div class="text">
-                        <?php if (mb_strlen(strip_tags(get_the_content()), 'utf-8') > 80) {
-                            $content = mb_substr(strip_tags(get_the_content()), 0, 80, 'utf-8');
-                            echo $content . "...";
-                        } else {
-                            echo strip_tags(get_the_content());
-                        }; ?>
-                    </div>
-                    <div class="readmore"><a href="<?php the_permalink() ?>">READ MORE</a></div>
-                </article>
-            <?php endwhile; ?>
-            <?php if (function_exists('pagination')) {
-                pagination($wp_query->max_num_pages);
-            }; ?>
-        <?php endif; ?>
+        <?php
+        if (have_posts()) :
+            get_template_part('loop');
 
+            if (function_exists("pagination")) {
+                pagination($wp_query->max_num_pages);
+            }
+        endif;
+        ?>
     </main>
     <?php get_sidebar(); ?>
 
--- wp-content/themes/practiceblog/search.php
+++ wp-content/themes/practiceblog/search.php
@@ -9,35 +9,13 @@
                 <h1 class="page-title">
                     「<?php echo $_GET['s']; ?>」の検索結果:<?php echo $wp_query->found_posts; ?>件
                 </h1>
-                <?php while (have_posts()) : the_post(); ?>
-                    <?php
-                    $cat = get_the_category();
-                    $catname = $cat[0]->cat_name;; ?>
-                    <article>
-                        <h2 class="article-title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-                        <ul class="meta">
-                            <li><?php the_date('Y/m/d'); ?></li>
-                            <li><?php $catname; ?></li>
+                <?php
+                get_template_part('loop');
 
-                        </ul>
-                        <a href="<?php the_permalink() ?>"><?php the_post_thumbnail(); ?></a>
-                        <div class="text">
-                            <?php
-                            if (mb_strlen(strip_tags(get_the_content()), 'UTF-8') > 80) {
-                                $content = mb_substr(strip_tags(get_the_content()), 0, 80, 'UTF-8');
-                                echo $content . '…';
-                            } else {
-                                echo strip_tags(get_the_content());
-                            }
-                            ?>
-                        </div>
-                        <div class="readmore"><a href="<?php the_permalink() ?>">READ MORE</a></div>
-                    </article>
-                <?php endwhile; ?>
-                <?php if (function_exists('pagination')) {
+                if (function_exists("pagination")) {
                     pagination($wp_query->max_num_pages);
-                }; ?>
-            <?php } ?>
+                }
+                ?> <?php } ?>
         <?php else : ?>
             <p>検索キーワードに一致する記事がありませんでした。</p>
         <?php endif; ?>
