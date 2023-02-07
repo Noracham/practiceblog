3f0100ed29ad8e519ec1126cfe2263be8dc22739
Nora <nsorym04@gmail.com>
2023-02-07 12:34:05 +0900

    修正途中

--- wp-content/themes/practiceblog/index.php
+++ wp-content/themes/practiceblog/index.php
@@ -3,32 +3,7 @@
 <div id="container" class="wrapper">
     <main>
         <?php if (have_posts()) : ?>
-            <?php while (have_posts()) : the_post(); ?>
-                <?php $cat = get_the_category(); ?>
-                <?php $catname = $cat[0]->cat_name; ?>
-
-                <article>
-                    <h2 class="article-title">
-                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                    </h2>
-                    <ul class="meta">
-                        <li><?php the_time("Y/m/d"); ?></li>
-                        <li><?php echo $catname; ?></li>
-                    </ul>
-                    <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
-                    <div class="text">
-                        <?php
-                        if (mb_strlen(strip_tags(get_the_content()), 'utf-8') > 80) {
-                            $content = mb_substr(strip_tags(get_the_content()), 0, 80, 'utf-8');
-                            echo $content . '...';
-                        } else {
-                            echo strip_tags(get_the_content());
-                        }
-                        ?>
-                    </div>
-                    <div class="readmore"><a href="<?php the_permalink($post) ?>">READ MORE</a></div>
-                </article>
-            <?php endwhile; ?>
+            <?php get_template_part('loop'); ?>
             <?php if (function_exists("pagination")) {
                 pagination($wp_query->max_num_pages);
             } ?>
